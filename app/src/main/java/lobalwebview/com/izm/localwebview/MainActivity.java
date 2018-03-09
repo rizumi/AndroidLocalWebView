cad8b5834a976017fab4ab3b3361ad2e16d36602
izumi <rizumi222@gmail.com>
2018-03-09 17:59:56 +0900

    Add Project

---
+++ app/src/main/java/lobalwebview/com/izm/localwebview/MainActivity.java
@@ -0,0 +1,20 @@
+package lobalwebview.com.izm.localwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
